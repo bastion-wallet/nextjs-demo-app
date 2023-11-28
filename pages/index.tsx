44105b59b7e7726210d0460b51db9d714ce7dd4f
Nikhil Asrani <asrani.nikhil@gmail.com>
2023-11-28 11:56:53 +0530

    updated to test with v2 of sdk

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,127 +1,152 @@
 import { useState, useEffect } from "react";
 import { ethers, Contract } from "ethers";
-import { Bastion } from "bastion-wallet-sdk";
+import { Bastion } from "k-test-2";
 import { ParticleNetwork } from "@particle-network/auth";
 import { ParticleProvider } from "@particle-network/provider";
 
 export default function Home() {
-	const [address, setAddress] = useState<string>("");
-	const [bastionConnect, setBastionConnect] = useState<any>();
-	const [isMinting, setIsMinting] = useState<boolean>(false);
-	const [userOpHash, setUserOpHash] = useState<string>("");
-	const [smartWalletAddress, setSmartWalletAddress] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [bastionConnect, setBastionConnect] = useState<any>();
+  const [isMinting, setIsMinting] = useState<boolean>(false);
+  const [userOpHash, setUserOpHash] = useState<string>("");
+  const [smartWalletAddress, setSmartWalletAddress] = useState<string>("");
 
-	useEffect(() => {
-		if (window.ethereum) {
-			window.ethereum.on("accountsChanged", (accounts: string[]) => {
-				setAddress(accounts[0]);
-			});
-		}
-	}, []);
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts: string[]) => {
+        setAddress(accounts[0]);
+      });
+    }
+  }, []);
 
-	const loginWithMetamask = async () => {
-		try {
-			// Check if Metamask is installed
-			if (!window.ethereum || !window.ethereum.isMetaMask) {
-				alert("Please install Metamask.");
-				return;
-			}
+  const loginWithMetamask = async () => {
+    try {
+      // Check if Metamask is installed
+      if (!window.ethereum || !window.ethereum.isMetaMask) {
+        alert("Please install Metamask.");
+        return;
+      }
 
-			// Request account access
-			const accounts = await window.ethereum.request({
-				method: "eth_requestAccounts",
-			});
+      // Request account access
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
 
-			setAddress(accounts[0]);
+      setAddress(accounts[0]);
 
-			const tempProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+      const tempProvider = new ethers.providers.Web3Provider(
+        window.ethereum,
+        "any"
+      );
 
-			const bastion = new Bastion();
-			const bastionConnect = await bastion.bastionConnect;
+      const bastion = new Bastion();
+      const bastionConnect = await bastion.bastionConnect;
 
-			await bastionConnect.init(tempProvider, {
-				apiKey: process.env.NEXT_PUBLIC_BASTION_API_KEY || "",
-			});
+      const response = await bastionConnect.init(tempProvider, {
+        apiKey: process.env.NEXT_PUBLIC_BASTION_API_KEY || "",
+      });
 
-			setBastionConnect(bastionConnect);
-			setSmartWalletAddress(await bastionConnect.getAddress());
-		} catch (e) {
-			console.error(e);
-		}
-	};
+      if (!response.exists) {
+        await bastionConnect.createSmartAccountByDapp();
+      }
 
-	const loginWithParticleAuth = async () => {
-		try {
-			const particle = new ParticleNetwork({
-				projectId: process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID as string,
-				clientKey: process.env.NEXT_PUBLIC_PARTICLE_CLIENT_KEY as string,
-				appId: process.env.NEXT_PUBLIC_PARTICLE_APP_ID as string,
-				chainName: "arbitrum",
-				chainId: 421613,
-			});
+      setBastionConnect(bastionConnect);
+      const addressResponse = await bastionConnect.getAddress();
+      // TODO change when BE changes ethers response
+      //   const address = addressResponse?.smartAccountAddress
+      setSmartWalletAddress(addressResponse);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-			const userInfo = await particle.auth.login();
-			const particleProvider = new ParticleProvider(particle.auth);
-			const tempProvider = new ethers.providers.Web3Provider(particleProvider, "any");
-			const address = await tempProvider.getSigner().getAddress();
-			setAddress(address);
+  const loginWithParticleAuth = async () => {
+    try {
+      const particle = new ParticleNetwork({
+        projectId: process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID as string,
+        clientKey: process.env.NEXT_PUBLIC_PARTICLE_CLIENT_KEY as string,
+        appId: process.env.NEXT_PUBLIC_PARTICLE_APP_ID as string,
+        chainName: "arbitrum",
+        chainId: 421613,
+      });
 
-			const bastion = new Bastion();
-			const bastionConnect = await bastion.bastionConnect;
+      const userInfo = await particle.auth.login();
+      const particleProvider = new ParticleProvider(particle.auth);
+      const tempProvider = new ethers.providers.Web3Provider(
+        particleProvider,
+        "any"
+      );
+      const address = await tempProvider.getSigner().getAddress();
+      setAddress(address);
 
-			await bastionConnect.init(tempProvider, {
-				apiKey: process.env.NEXT_PUBLIC_BASTION_API_KEY || "",
-			});
+      const bastion = new Bastion();
+      const bastionConnect = await bastion.bastionConnect;
 
-			setBastionConnect(bastionConnect);
-		} catch (e) {
-			console.error(e);
-		}
-	};
+      await bastionConnect.init(tempProvider, {
+        apiKey: process.env.NEXT_PUBLIC_BASTION_API_KEY || "",
+      });
 
-	const mintNFT = async () => {
-		try {
-			setIsMinting(true);
-			const contractAddress = "0xEAC57C1413A2308cd03eF3CEa5c9224487825341";
-			const contractABI = ["function safeMint(address to) public"];
+      setBastionConnect(bastionConnect);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-			const nftContract = new Contract(contractAddress, contractABI, bastionConnect);
+  const mintNFT = async () => {
+    try {
+      setIsMinting(true);
+      const contractAddress = "0xEAC57C1413A2308cd03eF3CEa5c9224487825341";
+      const contractABI = ["function safeMint(address to) public"];
 
-			const res = await nftContract.safeMint(address);
-			await res.wait();
-			console.log(res);
-			setIsMinting(false);
-			setUserOpHash(res.hash);
-		} catch (e) {
-			console.error(e);
-		}
-	};
+      const nftContract = new Contract(
+        contractAddress,
+        contractABI,
+        bastionConnect
+      );
 
-	return (
-		<div>
-			{address ? (
-				<div>
-					<h1>Welcome to Bastion!</h1>
-					<h4>Logged in as {address}</h4>
-					<h4>Smart Wallet - {smartWalletAddress}</h4>
-					{isMinting && <h3>Minting...</h3>}
-					{userOpHash && <h3>Minted NFT! User Operation Hash: {userOpHash}</h3>}
-					<button onClick={mintNFT} className="rounded-2 bg-gradient-to-r from-[#6C1EB0] to-[#DE389F] mx-4 my-4 px-10 py-4 h-full rounded-xl">
-						Mint NFT
-					</button>
-				</div>
-			) : (
-				<div>
-					<h1>Welcome to Bastion!</h1>
-					<button onClick={loginWithParticleAuth} className="rounded-2 bg-gradient-to-r from-[#6C1EB0] to-[#DE389F] mx-4 my-4 px-10 py-4 h-full rounded-xl">
-						Login with Particle Auth
-					</button>
-					<button onClick={loginWithMetamask} className="rounded-2 bg-gradient-to-r from-[#6C1EB0] to-[#DE389F] mx-4 my-4 px-10 py-4 h-full rounded-xl">
-						Login with Metamask
-					</button>
-				</div>
-			)}
-		</div>
-	);
+      const res = await nftContract.safeMint(address);
+      await res.wait();
+      console.log(res);
+      setIsMinting(false);
+      setUserOpHash(res.hash);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      {address ? (
+        <div>
+          <h1>Welcome to Bastion!</h1>
+          <h4>Logged in as {address}</h4>
+          <h4>Smart Wallet - {smartWalletAddress}</h4>
+          {isMinting && <h3>Minting...</h3>}
+          {userOpHash && <h3>Minted NFT! User Operation Hash: {userOpHash}</h3>}
+          <button
+            onClick={mintNFT}
+            className="rounded-2 bg-gradient-to-r from-[#6C1EB0] to-[#DE389F] mx-4 my-4 px-10 py-4 h-full rounded-xl"
+          >
+            Mint NFT
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h1>Welcome to Bastion!</h1>
+          <button
+            onClick={loginWithParticleAuth}
+            className="rounded-2 bg-gradient-to-r from-[#6C1EB0] to-[#DE389F] mx-4 my-4 px-10 py-4 h-full rounded-xl"
+          >
+            Login with Particle Auth
+          </button>
+          <button
+            onClick={loginWithMetamask}
+            className="rounded-2 bg-gradient-to-r from-[#6C1EB0] to-[#DE389F] mx-4 my-4 px-10 py-4 h-full rounded-xl"
+          >
+            Login with Metamask
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
-
