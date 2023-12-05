00f1febe5508bb92d001bde3f5a071ff667c8fbe
Nikhil Asrani <asrani.nikhil@gmail.com>
2023-12-05 16:56:04 +0530

    added viem support and updated to latest bastion-wallet-sdk

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 import { ethers, Contract } from "ethers";
-import { Bastion } from "k-test-2";
+import { Bastion } from "bastion-wallet-sdk";
 import { ParticleNetwork } from "@particle-network/auth";
 import { ParticleProvider } from "@particle-network/provider";
+import { createPublicClient, createWalletClient, custom } from "viem";
+import { polygonMumbai } from "viem/chains";
 
 export default function Home() {
   const [address, setAddress] = useState<string>("");
   const [bastionConnect, setBastionConnect] = useState<any>();
+  const [viemConnect, setViemConnect] = useState<any>();
+  const [bastionProvider, setBastionProvider] = useState<"ethers" | "viem">();
   const [isMinting, setIsMinting] = useState<boolean>(false);
   const [userOpHash, setUserOpHash] = useState<string>("");
   const [smartWalletAddress, setSmartWalletAddress] = useState<string>("");
@@ -19,7 +23,7 @@ export default function Home() {
     }
   }, []);
 
-  const loginWithMetamask = async () => {
+  const loginWithMetamask = async (bastionProvider: "ethers" | "viem") => {
     try {
       // Check if Metamask is installed
       if (!window.ethereum || !window.ethereum.isMetaMask) {
@@ -34,33 +38,34 @@ export default function Home() {
 
       setAddress(accounts[0]);
 
-      const tempProvider = new ethers.providers.Web3Provider(
-        window.ethereum,
-        "any"
-      );
+      if (bastionProvider === "ethers") {
+        const provider = new ethers.providers.Web3Provider(
+          window.ethereum,
+          "any"
+        );
 
-      const bastion = new Bastion();
-      const bastionConnect = await bastion.bastionConnect;
-
-      const response = await bastionConnect.init(tempProvider, {
-        apiKey: process.env.NEXT_PUBLIC_BASTION_API_KEY || "",
-      });
-
-      if (!response.exists) {
-        await bastionConnect.createSmartAccountByDapp();
+        const res = await loginWithBastionEthers(provider);
       }
+      if (bastionProvider === "viem") {
+        const publicClient = createPublicClient({
+          chain: polygonMumbai,
+          transport: custom(window.ethereum),
+        });
 
-      setBastionConnect(bastionConnect);
-      const addressResponse = await bastionConnect.getAddress();
-      // TODO change when BE changes ethers response
-      //   const address = addressResponse?.smartAccountAddress
-      setSmartWalletAddress(addressResponse);
+        const walletClient = createWalletClient({
+          chain: polygonMumbai,
+          transport: custom(window.ethereum),
+          account: accounts[0],
+        });
+        const res = await loginWithBastionViem(publicClient, walletClient);
+        console.log(res);
+      }
     } catch (e) {
       console.error(e);
     }
   };
 
-  const loginWithParticleAuth = async () => {
+  const loginWithParticleAuth = async (bastionProvider: "ethers" | "viem") => {
     try {
       const particle = new ParticleNetwork({
         projectId: process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID as string,
@@ -70,39 +75,85 @@ export default function Home() {
         chainId: 80001,
       });
 
-      const userInfo = await particle.auth.login();
-      const particleProvider = new ParticleProvider(particle.auth);
-      const tempProvider = new ethers.providers.Web3Provider(
-        particleProvider,
-        "any"
-      );
-      const signer = await tempProvider.getSigner();
-      const address = await signer.getAddress();
-      setAddress(address);
-
-      const bastion = new Bastion();
-      const bastionConnect = await bastion.bastionConnect;
-
-      const response = await bastionConnect.init(tempProvider, {
-        apiKey: process.env.NEXT_PUBLIC_BASTION_API_KEY || "",
+      const userInfo = await particle.auth.login({
+        preferredAuthType: "google",
       });
-
-      if (!response.exists) {
-        await bastionConnect.createSmartAccountByDapp();
+      console.log({ userInfo });
+      const particleProvider = new ParticleProvider(particle.auth);
+      if (bastionProvider === "ethers") {
+        const provider = new ethers.providers.Web3Provider(
+          particleProvider,
+          "any"
+        );
+        const isLogin = particle.auth.isLogin();
+        console.log({ isLogin });
+        const res = await loginWithBastionEthers(provider);
+        console.log(res);
       }
 
-      setBastionConnect(bastionConnect);
-      const addressResponse = await bastionConnect.getAddress();
-      // TODO change when BE changes ethers response
-      //   const address = addressResponse?.smartAccountAddress
-      setSmartWalletAddress(addressResponse);
+      if (bastionProvider === "viem") {
+        const [account] = await particleProvider.request({
+          method: "eth_accounts",
+        });
+        console.log("EOA PARTICLE AUTH ADDRESS:", account);
 
-      setBastionConnect(bastionConnect);
+        const publicClient = createPublicClient({
+          chain: polygonMumbai,
+          transport: custom(particleProvider),
+        });
+
+        const walletClient = createWalletClient({
+          chain: polygonMumbai,
+          transport: custom(particleProvider),
+          account,
+        });
+        const res = await loginWithBastionViem(publicClient, walletClient);
+      }
     } catch (e) {
       console.error(e);
     }
   };
 
+  const loginWithBastionEthers = async (provider: any) => {
+    setBastionProvider("ethers");
+    const bastion = new Bastion();
+    const bastionConnect = await bastion.bastionConnect;
+
+    const response = await bastionConnect.init(provider, {
+      apiKey: process.env.NEXT_PUBLIC_BASTION_API_KEY || "",
+    });
+
+    if (!response.exists) {
+      await bastionConnect.createSmartAccountByDapp();
+    }
+    setBastionConnect(bastionConnect);
+    const addressResponse = await bastionConnect.getAddress();
+    // TODO change when BE changes ethers response
+    // const address = addressResponse?.smartAccountAddress
+    // setSmartWalletAddress(addressResponse);
+    console.log(addressResponse);
+    setBastionConnect(bastionConnect);
+  };
+
+  const loginWithBastionViem = async (publicClient: any, walletClient: any) => {
+    setBastionProvider("viem");
+    const bastion = new Bastion();
+    const viemConnect = await bastion.viemConnect;
+    const CONFIG = {
+      chainId: 80001,
+      apiKey: process.env.NEXT_PUBLIC_BASTION_API_KEY || "",
+    };
+    const res = await viemConnect.init(publicClient, walletClient, CONFIG);
+    console.log({ res });
+    if (!res.exists) {
+      const createSmartAccountResult =
+        await viemConnect.createSmartAccountByDapp();
+    }
+    setViemConnect(viemConnect);
+    const bastionAddress = await viemConnect.getAddress();
+    console.log({ bastionAddress });
+    return { viemConnect, bastionAddress: bastionAddress.smartAccountAddress };
+  };
   const mintNFT = async () => {
     try {
       setIsMinting(true);
@@ -145,16 +196,28 @@ export default function Home() {
         <div>
           <h1>Welcome to Bastion!</h1>
           <button
-            onClick={loginWithParticleAuth}
+            onClick={() => loginWithParticleAuth("ethers")}
             className="rounded-2 bg-gradient-to-r from-[#6C1EB0] to-[#DE389F] mx-4 my-4 px-10 py-4 h-full rounded-xl"
           >
-            Login with Particle Auth
+            Login with Particle Auth (Ethers.js)
           </button>
           <button
-            onClick={loginWithMetamask}
+            onClick={() => loginWithMetamask("ethers")}
             className="rounded-2 bg-gradient-to-r from-[#6C1EB0] to-[#DE389F] mx-4 my-4 px-10 py-4 h-full rounded-xl"
           >
-            Login with Metamask
+            Login with Metamask (Ethers.js)
+          </button>
+          <button
+            onClick={() => loginWithParticleAuth("viem")}
+            className="rounded-2 bg-gradient-to-r from-[#6C1EB0] to-[#DE389F] mx-4 my-4 px-10 py-4 h-full rounded-xl"
+          >
+            Login with Particle Auth (Viem)
+          </button>
+          <button
+            onClick={() => loginWithMetamask("viem")}
+            className="rounded-2 bg-gradient-to-r from-[#6C1EB0] to-[#DE389F] mx-4 my-4 px-10 py-4 h-full rounded-xl"
+          >
+            Login with Metamask (Viem)
           </button>
         </div>
       )}
